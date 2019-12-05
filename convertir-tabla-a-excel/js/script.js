2f67a862ebc4da9c9e36757c9900ac1af614a938
Luis Cabrera Benito <parzibyte@gmail.com>
2019-12-04 22:43:05 -0600

    Ejemplo para exportar HTML a Excel

---
+++ convertir-tabla-a-excel/js/script.js
@@ -0,0 +1,13 @@
+const $btnExportar = document.querySelector("#btnExportar"),
+    $tabla = document.querySelector("#tabla");
+
+$btnExportar.addEventListener("click", function() {
+    let tableExport = new TableExport($tabla, {
+        exportButtons: false, // No queremos botones
+        filename: "Mi tabla de Excel", //Nombre del archivo de Excel
+        sheetname: "Mi tabla de Excel", //Título de la hoja
+    });
+    let datos = tableExport.getExportData();
+    let preferenciasDocumento = datos.tabla.xlsx;
+    tableExport.export2file(preferenciasDocumento.data, preferenciasDocumento.mimeType, preferenciasDocumento.filename, preferenciasDocumento.fileExtension, preferenciasDocumento.merges, preferenciasDocumento.RTL, preferenciasDocumento.sheetname);
+});
