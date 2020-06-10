c9245db8726d1ccdd701ce4d91fb90efc2923fdd
Luis Cabrera Benito <parzibyte@gmail.com>
2020-06-10 15:31:06 -0500

    Cambiando resolución

--- quaggajs/script.js
+++ quaggajs/script.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const $resultados=document.querySelector("#resultados");
     Quagga.init({
         inputStream : {
+          constraints:{
+            width: 1920,
+            height: 1080,
+          },
           name : "Live",
           type : "LiveStream",
           target: document.querySelector('#contenedor')    // Or '#yourElement' (optional)
