fb6f8b9cb986759c903fda394c5f3aa3a0d4ab02
Luis Cabrera Benito <parzibyte@gmail.com>
2020-06-10 18:52:13 -0500

    Ejemplo funcional de QuaggaJS

--- quaggajs/script.js
+++ quaggajs/script.js
@@ -1,29 +1,13 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  const $resultados=document.querySelector("#resultados");
-    Quagga.init({
-        inputStream : {
-          constraints:{
-            width: 1280,
-            height: 720,
-          },
-          name : "Live",
-          type : "LiveStream",
-          target: document.querySelector('#contenedor')    // Or '#yourElement' (optional)
-        },
-        decoder : {
-          readers : ["ean_reader"]
-        }
-      }, function(err) {
-          if (err) {
-              console.log(err);
-              return
-          }
-          console.log("Initialization finished. Ready to start");
-          Quagga.start();
-      });
+document.addEventListener("DOMContentLoaded", () => {
+	const $btnEscanear = document.querySelector("#btnEscanear"),
+	$input = document.querySelector("#codigo");
+	$btnEscanear.addEventListener("click", ()=>{
+		window.open("leer.html");
+	});
 
-      Quagga.onDetected((data)=>{
-        $resultados.innerHTML = JSON.stringify(data);
-          console.log({data});
-      });
+	window.onCodigoLeido = datosCodigo => {
+		console.log("Oh sí, código leído: ")
+		console.log(datosCodigo)
+		$input.value = datosCodigo.codeResult.code;
+	}
 });
---
+++ quaggajs/scriptLeer.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const $resultados = document.querySelector("#resultados");
+	Quagga.init({
+		inputStream: {
+			constraints: {
+				width: 1920,
+				height: 1080,
+			},
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector('#contenedor')    // Or '#yourElement' (optional)
+		},
+		decoder: {
+			readers: ["ean_reader"]
+		}
+	}, function (err) {
+		if (err) {
+			console.log(err);
+			return
+		}
+		console.log("Initialization finished. Ready to start");
+		Quagga.start();
+	});
+
+	Quagga.onDetected((data) => {
+		if(window.opener){
+			window.opener.onCodigoLeido(data);
+			window.close();
+		}
+	});
+
+	Quagga.onProcessed(function (result) {
+		var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+				result.boxes.filter(function (box) {
+					return box !== result.box;
+				}).forEach(function (box) {
+					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+				});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+			}
+
+			if (result.codeResult && result.codeResult.code) {
+				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+			}
+		}
+	});
+});
