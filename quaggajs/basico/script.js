4e26fa8f216b6d157f5c19ed74ce97f2709e9c83
Luis Cabrera Benito <parzibyte@gmail.com>
2020-06-21 23:58:04 -0500

    Agregadas opciones simples

---
+++ quaggajs/basico/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const $resultados = document.querySelector("#resultado");
+	Quagga.init({
+		inputStream: {
+			constraints: {
+				width: 1920,
+				height: 1080,
+			},
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector('#contenedor'), // Pasar el elemento del DOM
+		},
+		decoder: {
+			readers: ["ean_reader"]
+		}
+	}, function (err) {
+		if (err) {
+			console.log(err);
+			return
+		}
+		console.log("Iniciado correctamente");
+		Quagga.start();
+	});
+
+	Quagga.onDetected((data) => {
+		$resultados.textContent = data.codeResult.code;
+		// Imprimimos todo el data para que puedas depurar
+		console.log(data);
+	});
+});
---
+++ quaggajs/con-dibujo/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const $resultados = document.querySelector("#resultado");
+	Quagga.init({
+		inputStream: {
+			constraints: {
+				width: 1920,
+				height: 1080,
+			},
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector('#contenedor'), // Pasar el elemento del DOM
+		},
+		decoder: {
+			readers: ["ean_reader"]
+		}
+	}, function (err) {
+		if (err) {
+			console.log(err);
+			return
+		}
+		console.log("Iniciado correctamente");
+		Quagga.start();
+	});
+
+	Quagga.onDetected((data) => {
+		$resultados.textContent = data.codeResult.code;
+		// Imprimimos todo el data para que puedas depurar
+		console.log(data);
+	});
+
+	Quagga.onProcessed(function (result) {
+		var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+				result.boxes.filter(function (box) {
+					return box !== result.box;
+				}).forEach(function (box) {
+					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+				});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+			}
+
+			if (result.codeResult && result.codeResult.code) {
+				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+			}
+		}
+	});
+});
