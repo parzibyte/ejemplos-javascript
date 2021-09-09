26e42cb9ed5a53da975fc3eec227c5ee644f83ab
Luis Cabrera Benito <parzibyte@gmail.com>
2021-09-09 12:01:40 -0500

    Ejemplo para firma de usuario

---
+++ firma-js/script.js
@@ -0,0 +1,110 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+            | IMPORTANTE |
+Si vas a borrar este encabezado, considera:
+Seguirme: https://parzibyte.me/blog/sigueme/
+Y compartir mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+Twitter: https://twitter.com/parzibyte
+Facebook: https://facebook.com/parzibyte.fanpage
+Instagram: https://instagram.com/parzibyte
+Hacer una donación vía PayPal: https://paypal.me/LuisCabreraBenito
+------------------------------------------------------------------------------------------------
+*/
+const $canvas = document.querySelector("#canvas"),
+    $btnDescargar = document.querySelector("#btnDescargar"),
+    $btnLimpiar = document.querySelector("#btnLimpiar"),
+    $btnGenerarDocumento = document.querySelector("#btnGenerarDocumento");
+const contexto = $canvas.getContext("2d");
+const COLOR_PINCEL = "black";
+const COLOR_FONDO = "white";
+const GROSOR = 2;
+let xAnterior = 0, yAnterior = 0, xActual = 0, yActual = 0;
+const obtenerXReal = (clientX) => clientX - $canvas.getBoundingClientRect().left;
+const obtenerYReal = (clientY) => clientY - $canvas.getBoundingClientRect().top;
+let haComenzadoDibujo = false; // Bandera que indica si el usuario está presionando el botón del mouse sin soltarlo
+
+
+const limpiarCanvas = () => {
+    // Colocar color blanco en fondo de canvas
+    contexto.fillStyle = COLOR_FONDO;
+    contexto.fillRect(0, 0, $canvas.width, $canvas.height);
+};
+limpiarCanvas();
+$btnLimpiar.onclick = limpiarCanvas;
+// Escuchar clic del botón para descargar el canvas
+$btnDescargar.onclick = () => {
+    const enlace = document.createElement('a');
+    // El título
+    enlace.download = "Firma.png";
+    // Convertir la imagen a Base64 y ponerlo en el enlace
+    enlace.href = $canvas.toDataURL();
+    // Hacer click en él
+    enlace.click();
+};
+
+window.obtenerImagen = () => {
+    return $canvas.toDataURL();
+};
+
+$btnGenerarDocumento.onclick = () => {
+    const ventana = window.open("documento.html");
+};
+// Lo demás tiene que ver con pintar sobre el canvas en los eventos del mouse
+$canvas.addEventListener("mousedown", evento => {
+    // En este evento solo se ha iniciado el clic, así que dibujamos un punto
+    xAnterior = xActual;
+    yAnterior = yActual;
+    xActual = obtenerXReal(evento.clientX);
+    yActual = obtenerYReal(evento.clientY);
+    contexto.beginPath();
+    contexto.fillStyle = COLOR_PINCEL;
+    contexto.fillRect(xActual, yActual, GROSOR, GROSOR);
+    contexto.closePath();
+    // Y establecemos la bandera
+    haComenzadoDibujo = true;
+});
+
+$canvas.addEventListener("mousemove", (evento) => {
+    if (!haComenzadoDibujo) {
+        return;
+    }
+    // El mouse se está moviendo y el usuario está presionando el botón, así que dibujamos todo
+
+    xAnterior = xActual;
+    yAnterior = yActual;
+    xActual = obtenerXReal(evento.clientX);
+    yActual = obtenerYReal(evento.clientY);
+    contexto.beginPath();
+    contexto.moveTo(xAnterior, yAnterior);
+    contexto.lineTo(xActual, yActual);
+    contexto.strokeStyle = COLOR_PINCEL;
+    contexto.lineWidth = GROSOR;
+    contexto.stroke();
+    contexto.closePath();
+});
+["mouseup", "mouseout"].forEach(nombreDeEvento => {
+    $canvas.addEventListener(nombreDeEvento, () => {
+        haComenzadoDibujo = false;
+    });
+});
